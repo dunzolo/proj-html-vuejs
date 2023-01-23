4259435656b51123c731a776c5cdbc6a0dacb18f
dunzolo <rossidavide108@gmail.com>
2023-01-23 16:02:52 +0100

    added content jumbotron-left container

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -8,6 +8,18 @@ export default {
     <div class="jumbotron">
         <div class="container-full">
             <div class="jumbotron-left">
+                <div class="container">
+                    <div class="col-3">
+                        <span>________</span>
+                    </div>
+                    <div class="col-9">
+                        <p>THE BEST TABLE IN TOWN</p>
+                        <h1>FINE DINING EXPERIENCE</h1>
+                        <p>Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.</p>
+                        <button>EXPLORE THE MENU</button>
+                        
+                    </div>
+                </div>
             </div>
             <div class="jumbotron-right">
                 <img src="/slider52x.jpg" alt="image-jumbotron">
@@ -38,8 +50,20 @@ export default {
 
         .jumbotron-left {
             background-color: black;
+            color: white;
             width: 50%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
 
+            .container {
+                display: flex;
+                padding: 10rem;
+
+                h1 {
+                    font-size: 80px;
+                }
+            }
 
         }
     }
