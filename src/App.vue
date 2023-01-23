648f58ceefe281535c37c9238e1d14e726bc7b45
dunzolo <rossidavide108@gmail.com>
2023-01-23 14:52:42 +0100

    added header and navbar with props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <script>
-export default {
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      array_navbar: [
+        {
+          label: 'Home',
+          url: '#'
+        },
+        {
+          label: 'History',
+          url: '#'
+        },
+        {
+          label: 'Team',
+          url: '#'
+        },
+        {
+          label: 'Menu',
+          url: '#'
+        },
+        {
+          label: 'Takeout',
+          url: '#'
+        },
+        {
+          label: 'Bulletin',
+          url: '#'
+        },
+        {
+          label: 'Reservation',
+          url: '#'
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <template lang="">
   <div>
-    
+    <AppHeader :navbar_list="array_navbar"/>
   </div>
 </template>
 
-<style lang="">
-  
+<style lang="scss">
+@use './style/generals.scss'
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    props: {
+        navbar_list: Object
+    }
+}
+</script>
+
+<template lang="">
+    <header>
+        <div class="container-header">
+            <div class="logo">
+                <img src="/logo-restaurant-2x-200x38.png" alt="logo">
+            </div>
+            <div>
+                <ul class="unstyled-list">
+                    <li v-for="(item, index) in navbar_list" :key="index">
+                        <a href="">
+                            {{ item.label }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: black;
+
+    .container-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 90%;
+        margin: 0 auto;
+        padding: 2rem;
+
+        ul {
+            list-style-type: none;
+            display: flex;
+
+            li a {
+                text-decoration: none;
+                color: white;
+                padding: 10px;
+            }
+        }
+    }
+
+}
+</style>
