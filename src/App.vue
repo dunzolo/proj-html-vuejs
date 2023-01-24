36be7492c7885fdd84a7eb3ab916d302da16859d
dunzolo <rossidavide108@gmail.com>
2023-01-24 17:12:26 +0100

    styled dropdown menu navbar header

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,16 @@ export default {
         {
           label: 'Home',
           url: '#',
-          tag_list: [
+          dropdown_list: [
             'Alternate Homepage 1', 'Alternate Homepage 2'
           ],
-          isVisibile: false
+          is_visible: false
 
         },
         {
           label: 'History',
           url: '#',
-          dropdown_menu: [
+          dropdown_list: [
             'Reception & Bar'
           ]
         },
@@ -34,12 +34,38 @@ export default {
         },
         {
           label: 'Menu',
-          url: '#'
+          url: '#',
+          dropdown_box: [
+            {
+              title: 'titolo',
+              icon: 'fa-solid fa-plate-wheat fa-5x',
+              paragraph: 'paragrafo',
+              background_image: '/mm-1.jpg'
+            },
+            {
+              title: 'titolo',
+              icon: 'fa-solid fa-bowl-rice fa-5x',
+              paragraph: 'paragrafo',
+              background_image: '/mm-2.jpg'
+            },
+            {
+              title: 'titolo',
+              icon: 'fa-solid fa-cookie-bite fa-5x',
+              paragraph: 'paragrafo',
+              background_image: '/mm-3.jpg'
+            },
+            {
+              title: 'titolo',
+              icon: 'fa-solid fa-martini-glass-citrus fa-5x',
+              paragraph: 'paragrafo',
+              background_image: '/mm-4.jpg'
+            },
+          ]
         },
         {
           label: 'Takeout',
           url: '#',
-          dropdown_menu: [
+          dropdown_list: [
             'Simple Product', 'Variable Product'
           ]
         },
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,6 +3,16 @@ export default {
     props: {
         navbar_list: Object
     },
+    data() {
+        return {
+            index_visible: ''
+        }
+    },
+    methods: {
+        showDropdown(index) {
+            this.index_visible = index
+        }
+    },
 }
 </script>
 
@@ -14,12 +24,22 @@ export default {
             </div>
             <div>
                 <ul class="unstyled-list">
-                    <li v-for="(item, index) in navbar_list" :key="index" @click="item.isVisible = true">
+                    <li v-for="(item, index) in navbar_list" :key="index" @mouseover="showDropdown(index)">
                         <a>
                             {{ item.label }}
-                            <ul v-if="item.isVisible">
-                                <li v-for="(item, index) in item.tag_list" :key="index">
+                            <ul class="dropdown-list" v-if="item.dropdown_list && index_visible === index">
+                                <li v-for="(item, index) in item.dropdown_list" :key="index">
                                     {{ item }}
+                                </li>
+                            </ul>
+                            <ul class="dropdown-box" v-if="item.dropdown_box && index_visible === index">
+                                <li class="box" v-for="(item, index) in item.dropdown_box" :key="index">
+                                    <div class="box-card" :style="{ 'background-image': `url(${item.background_image})`}">
+                                        <h1>{{ item.title }}</h1>
+                                        <i :class= item.icon></i>
+                                        <p>{{ item.paragraph }}</p>
+                                        <i class="fa-thin fa-sushi"></i>
+                                    </div>
                                 </li>
                             </ul>
                         </a>
@@ -42,7 +62,9 @@ header {
         justify-content: space-between;
         width: 90%;
         margin: 0 auto;
-        padding: 2rem;
+        padding: 2rem 2rem 0 2rem;
+        position: relative;
+
 
         .logo img {
             width: 250px;
@@ -52,18 +74,56 @@ header {
             list-style-type: none;
             display: flex;
             margin-bottom: 0px; //bootstrap aggiunge di default margin-bottom
+            cursor: pointer;
 
             li a {
                 text-decoration: none;
                 color: white;
-                padding: 10px;
+                padding: 30px;
             }
 
-            .dropdown {
-                background-color: red;
-                height: 100px;
-                width: 100px;
-                border: 1px solid black
+            .dropdown-list {
+                background-color: #3B3B3B;
+                position: absolute;
+                padding: 0px;
+                list-style-type: none;
+                top: 100%;
+                border-radius: 5px;
+
+
+                li {
+                    padding: 1rem;
+
+                    &:hover {
+                        background-color: #BDA16B;
+                        border-radius: 5px;
+
+
+                    }
+                }
+            }
+
+            .dropdown-box {
+                position: absolute;
+                padding: 0;
+                top: calc(100% + 20px);
+                left: 0;
+                list-style-type: none;
+                display: flex;
+                width: 100%;
+
+                .box {
+                    width: calc(100% / 4);
+
+                    .box-card {
+                        background-image: linear-gradient(black, transparent);
+                        background-size: 100%;
+                        height: 400px;
+                        text-align: center;
+                        padding: 3rem;
+                    }
+
+                }
             }
         }
     }
